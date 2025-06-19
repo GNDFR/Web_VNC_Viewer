60431c4946267d3fc2610d2088653b5582cbfdcb
GNDFR <kimhyowoo435@gmail.com>
2025-06-19 18:06:35 +0900

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// webpack.config.js
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client.tsx", // 실제 파일명이 이게 맞는지 꼭 확인!
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: "production",
+};
